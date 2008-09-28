2fc857a726788a919932b5036f18250c9c28b3ed
Will Larson <lethain@gmail.com>
2008-09-28 11:15:46 -0400

    Initial implementation of model.

--- models.py
+++ models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    # Title of note.
+    title = models.CharField(max_length=200)
+    # Note's slug for its URL.
+    slug = models.SlugField(max_length=200)
+    # Tags for classifying note.
+    tags = models.CharField(max_length=200)
+    # The body of the note.
+    text = models.TextField(blank=True, null=True)
+    # When the note was created.
+    created = models.DateTimeField(auto_now_add=True)
+    # When the note 'started'. User definable.
+    start = models.DateTimeField(blank=True, null=True)
+    # When the note 'ended'. User definable. If there
+    # is no end date, then a project is 'ongoing'
+    end = models.DateTimeField(blank=True, null=True)
+    
+    # Typing Notes (text, snippet, etc)
+    type = models.CharField(max_length=200)
+    # For text, the markup. For snippet, programming language.
+    type_detail = models.CharField(max_length=200)
+
+    def get_absolute_url(self):
+        return u"/n/%s/" % self.slug
+
+    def __unicode__(self):
+        return u"Note(%s, %s)" % (self.title, self.created)
+
+    
+    
