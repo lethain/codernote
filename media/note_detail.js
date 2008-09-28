9efddb354461759bac50f321e69ee998ccada15a
Will Larson <lethain@gmail.com>
2008-09-28 18:32:55 -0400

    Added datepicker to select dates.

--- media/note_detail.js
+++ media/note_detail.js
@@ -3,15 +3,31 @@ $(document).ready(function() {
 	var input = $('<input class="large" value="'+this.innerHTML+'">');
 	input.hover(function(){},
 		    function() {
-		      var str = '<span id="'+this.id+'" class="editable">'+input.val()+'</span>';
+		      var str = '<span id="'+this.id+'" class="editable">';
+		      str += input.val()+'</span>';
 		      var new_input = $(str);
 		      new_input.click(make_spans_editable);
 		      input.replaceWith(new_input);
-		      
 		    });
-
 	$(this).replaceWith(input);
     };
     $("span.editable").click(make_spans_editable);
+
+    var start = '01/01/1996';
+
+    $("span.date").datePicker({createButton:false, startDate:start})
+      .bind(
+	    'click',
+	    function() {
+	      $(this).dpDisplay();
+	      this.blur();
+	      return false;
+	    })
+      .bind(
+	    'dateSelected',
+	    function(e, d, $td) {
+	      var str = d.getMonth() + "/" +d.getDate() +"/"+d.getYear();
+	      $(this).text(str);
+	    });
     
   });
